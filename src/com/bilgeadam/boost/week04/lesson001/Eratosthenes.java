e458aa1371e755bd5140dd959fffac9f3deb84ec
Ezgi Akkas <ezgituncalp@hotmail.com>
2022-09-10 00:17:31 +0300

    Break Homework

--- src/com/bilgeadam/boost/week04/lesson001/Eratosthenes.java
+++ src/com/bilgeadam/boost/week04/lesson001/Eratosthenes.java
@@ -1,44 +1,41 @@
 package com.bilgeadam.boost.week04.lesson001;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Eratosthenes {
 	
 	public static void main(String[] args) {
+		 Scanner scan = new Scanner(System.in);
+	     System.out.println("Enter n: ");
+	     if (!scan.hasNextInt()) {
+	        System.err.println("Please enter a number!");
+	        return;
+	     }
+         sieveOfEratosthenes(scan.nextInt());
+	}
+
+    private static void sieveOfEratosthenes(int n) {
+        boolean primes[] = new boolean[n+1];
+        for(int i=0; i<=n; i++) {
+            primes[i] = (i > 1);
+        }
+
+	    for (int i = 2; i < primes.length; i++) {
+	        if (primes[i] == true) {
+	            for (int j = 2; i * j < primes.length; j++) {
+	                primes[i*j] = false;
+	            }
+	        }
+	    }
 	
-		Scanner input = new Scanner(System.in);
-		System.out.println("enter a number :  ");
-		int n = input.nextInt();
-		int [] arrays = new int [n];
-		n = arrays.length;
-		input.close();
+	    for(int i = 1; i <= n; i++) {
+	        if(primes[i] == true) {
+	            System.out.print(i + " ");
+	        } else {
+	            System.out.print(i + "x ");
+	        }
+	    }
 	   
-		List<Integer> arraylist = new ArrayList<>();
-		       
-		
-		          
-		     for (int i=1; i<=n; i++) {
-		    	 arraylist.add(i);
-		    	 System.out.println(arraylist);
-		    	
-			 }
-		     
-	             
-		
-			
-		
-	}
-	  
-	}
-	
-
-		
-		
-		
-		
-
-	
-
-
+    }
+    
+}
--- src/com/bilgeadam/boost/week04/lesson001/Frantions.java
+++ src/com/bilgeadam/boost/week04/lesson001/Frantions.java
@@ -3,30 +3,28 @@ package com.bilgeadam.boost.week04.lesson001;
 import java.util.Scanner;
 
 public class Frantions {
-	private Scanner sc;
-	private int selection;
+
 
 	public static void main(String[] args) {
 	
 	
 		 prosess();
 		
-		
-		
 	}
 
 
+	
 	
 
 
 	private static void prosess() {
 		
 		Scanner sc = new Scanner(System.in);
+				
 		System.out.println("please enter first fraction of numerator: ");
 		int num1  =sc.nextInt();
 		System.out.println("please enter first fraction of denominator: ");
 		int denom1 = sc.nextInt();
-		
 		System.out.println("please enter second fraction of numerator: ");
 		int num2 = sc.nextInt();
 		System.out.println("please enter second fraction of denominator: ");
@@ -37,59 +35,107 @@ public class Frantions {
 		System.out.println("2 for subtraction");
 		System.out.println("3 for multiply");
 		System.out.println("4 for division");
-		int selection = sc.nextInt();
-		sc.close();
-	
+		if (!sc.hasNextInt()) {
+			System.err.println("please enter a NUMBER!!");
+		}else {
 		
+		int selection = sc.nextInt();
+		ebob(selection, num1, num2, denom1, denom2);
+		
+		sc.close();
+	  }
+	}
+	
+	private static void ebob(int selection, int num1, int num2, int denom1, int denom2) {
+
+		int temp1;
+		int hcf1 = 1;
+		
+		if(num1>denom1) {
+			temp1 = denom1;
+		}else {
+			temp1 = num1;
+		}
+		while(temp1>0) {
+			if(num1%temp1==0 && denom1%temp1==0) {
+				hcf1 = temp1;
+				break;
+			}
+			temp1--;
+		}		
+		temp1=1;
+		
+		num1 = num1 / hcf1;
+		denom1 = denom1 / hcf1;
+//		System.out.println(num1 + "/" + denom1);
+		
+		
+		int temp2;
+		int hcf2 = 1;
+		if(num2>denom2) {
+			temp2 = denom2;
+		}else {
+			temp2 = num2;
+		}
+		while(temp2>0) {
+			if(num2%temp2==0 && denom2%temp2==0) {
+				hcf2 = temp2;
+				break;
+			}
+			temp2--;
+		}		
+		temp2=1;
+		
+		num2 = num2 / hcf2;
+		denom2 = denom2 / hcf2;
+//		System.out.println(num2 + "/" + denom2);
+		frantionProsess(selection, num1, num2, denom1, denom2);
+	}
+
+	
+    private static void frantionProsess(int selection, int num1, int num2, int denom1, int denom2) {
 			
-			if (selection == 1 || selection == 2) {
-			       if(denom1 == denom2 ) {
-			    	   if (selection == 1) {
-				       int totalNum = num1 + num2;
-				       System.out.println("your frantions sub is: " + totalNum +"/"+ denom1);
-			    	   }else{
-			    		   int totalSub = num1 - num2;
-					     System.out.println("your frantions sub is: " + totalSub +"/"+ denom1);
-			    	   }
-			       } else {
+		if (selection == 1 || selection == 2) {
+			if(denom1 == denom2 ) {
+			   if (selection == 1) {
+				   int totalNum = num1 + num2;
+				   System.out.println("your frantions sub is: " + totalNum +"/"+ denom1);
+			     }else{
+			       int totalSub = num1 - num2;
+				   System.out.println("your frantions sub is: " + totalSub +"/"+ denom1);
+			       }
+			 } else {
 				
-				      int numTotal = num1*denom2 + num2 * denom1;
-				      int denomTotal = denom1 * denom2;
-				      if (selection == 1) {
+				   int numTotal = num1*denom2 + num2 * denom1;
+				   int denomTotal = denom1 * denom2;
+				   if (selection == 1) {
 				      System.out.println("your frantions sub is: " + numTotal +"/" + denomTotal );
-				      }else {
+				    }else {
 				      int subTotal = num1*denom2 - num2 * denom1;
 				      System.out.println("your frantions sub is: " + subTotal +"/" + denomTotal );
 				      }
-			       }
-			} 
-			else if(selection == 3) {
+			   }
+	    } 
+			
+		    else if(selection == 3) {
 			      int multiply1 = num1 * num2;
 				  int multiply2 = denom1 * denom2;
 				     System.out.println(multiply1 + "/" + multiply2);
-			       } 
+			 } 
 				  
 		    else if (selection == 4) {  
 			      int division1 = num1 * denom2;
 			      int division2 = denom1 * num2;
 			      System.out.println(division1 + "/" + division2);
-			       } 
+			 } 
 				
 			else {
 				  System.err.println("wrong! please try entry!");
-			       }
-
-		
+			 }
+			
+		 }
+	
 		
 		
      
-			
-	}	
-}    	
-
-
-	
-
-
-
-
+}   	
