b61e8405e1b76184d06101eb0a2b1a346a87bf0b
Ezgi Tuncalp <ezgituncalp@hotmail.com>
2022-11-06 21:04:37 +0300

    Test commit

--- src/com/bilgeadam/boost/week10/lesson001/ex4.java
+++ src/com/bilgeadam/boost/week10/lesson001/ex4.java
@@ -4,7 +4,7 @@ public class ex4 {
 
 	public static void main(String[] args) {
 
-		System.out.println("*");
+		System.out.println("*");	
 		
 		for (int i = 1; i <= 10; i += 2) {
 			for (int j = 2; j <= i; j++) {
